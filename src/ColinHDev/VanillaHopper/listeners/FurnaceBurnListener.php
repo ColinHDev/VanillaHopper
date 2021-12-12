f6aa21db7783a196d6ebdcc07dd0d9cf6a25e0d3
ColinHDev <colinheidfeld@gmail.com>
2021-12-12 23:16:18 +0100

    improve block update scheduling in furnace event listeners

--- src/ColinHDev/VanillaHopper/listeners/FurnaceBurnListener.php
+++ src/ColinHDev/VanillaHopper/listeners/FurnaceBurnListener.php
@@ -6,12 +6,29 @@ use ColinHDev\VanillaHopper\blocks\BlockUpdateScheduler;
 use ColinHDev\VanillaHopper\blocks\tiles\Hopper;
 use pocketmine\event\inventory\FurnaceBurnEvent;
 use pocketmine\event\Listener;
+use pocketmine\math\Facing;
 
 class FurnaceBurnListener implements Listener {
 
+    /**
+     * As hoppers can only affect the fuel slot of a furnace by either pushing into it
+     * (when the hopper is facing horizontally) or by pulling the item from the fuel slot (e.g. an empty bucket)
+     * (when the hopper is placed below), we don't need to schedule a delayed block update for the block above since
+     * that can only push into the furnace's input slot.
+     * @var int[]
+     */
+    private const FACINGS = [
+        Facing::DOWN,
+        Facing::NORTH,
+        Facing::SOUTH,
+        Facing::WEST,
+        Facing::EAST
+    ];
+
     public function onFurnaceBurn(FurnaceBurnEvent $event) : void {
         $position = $event->getFurnace()->getPosition();
-        foreach ($position->sides() as $vector3) {
+        foreach (self::FACINGS as $facing) {
+            $vector3 = $position->getSide($facing);
             $tile = $position->world->getTile($vector3);
             if (!$tile instanceof Hopper) {
                 continue;
--- src/ColinHDev/VanillaHopper/listeners/FurnaceSmeltListener.php
+++ src/ColinHDev/VanillaHopper/listeners/FurnaceSmeltListener.php
@@ -6,12 +6,27 @@ use ColinHDev\VanillaHopper\blocks\BlockUpdateScheduler;
 use ColinHDev\VanillaHopper\blocks\tiles\Hopper;
 use pocketmine\event\inventory\FurnaceSmeltEvent;
 use pocketmine\event\Listener;
+use pocketmine\math\Facing;
 
 class FurnaceSmeltListener implements Listener {
 
+    /**
+     * The FurnaceSmeltEvent is called when the furnace smelted an input item to a result item. If that is the case, the
+     * only hoppers that could be affected is the hopper above, which could now be able to push another input item into
+     * the furnace, and the hopper below, which could now be able to pull the result item out of the furnace. That's why
+     * we don't need the check the blocks horizontally placed to the furnace, as those hoppers could only push to the
+     * furnace's fuel slot.
+     * @var int[]
+     */
+    private const FACINGS = [
+        Facing::DOWN,
+        Facing::UP
+    ];
+
     public function onFurnaceSmelt(FurnaceSmeltEvent $event) : void {
         $position = $event->getFurnace()->getPosition();
-        foreach ($position->sides() as $vector3) {
+        foreach (self::FACINGS as $facing) {
+            $vector3 = $position->getSide($facing);
             $tile = $position->world->getTile($vector3);
             if (!$tile instanceof Hopper) {
                 continue;
